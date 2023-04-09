418027b535d3fdfed760818f2aead18df5c1f432
Corey <coreytruscott@tutanota.com>
2023-04-09 23:00:44 +0100

    change animate mini timings

--- lua/plugins/mini-animate.lua
+++ lua/plugins/mini-animate.lua
@@ -17,10 +17,10 @@ return {
       local animate = require("mini.animate")
       return {
         resize = {
-          timing = animate.gen_timing.linear({ duration = 30, unit = "total" }),
+          timing = animate.gen_timing.linear({ duration = 70, unit = "total" }),
         },
         scroll = {
-          timing = animate.gen_timing.linear({ duration = 70, unit = "total" }),
+          timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
           subscroll = animate.gen_subscroll.equal({
             predicate = function(total_scroll)
               if mouse_scrolled then
