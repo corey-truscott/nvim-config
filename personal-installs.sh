09268a922c1cac3418aa70867a0d279c7a99f288
coreytruscott8@gmail.com <coreytruscott8@gmail.com>
2023-04-15 12:08:43 +0100

    fix personal-installs.sh

--- personal-installs.sh
+++ personal-installs.sh
@@ -1,2 +1,2 @@
-nvim -c MasonInstall fixjson lua-language-server bash-language-server black clang-format clangd flake8 html-lsp json-lsp pyright rust-analyzer stylua rustfmt
-nvim -c TSInstall all
+nvim -c "TSInstall all"
+nvim -c "MasonInstall fixjson lua-language-server bash-language-server black clang-format clangd flake8 html-lsp json-lsp pyright rust-analyzer stylua rustfmt"
