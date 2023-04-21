13c2a6a49a226f7dfe82b19ad1e850b164aa0b31
coreytruscott8@gmail.com <coreytruscott8@gmail.com>
2023-04-22 00:15:59 +0100

    replace surround (tpope) with mini surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,4 +1,26 @@
 return {
-  "tpope/vim-surround",
+  "echasnovski/mini.surround",
+  version = '*',
   event = { "BufReadPre", "BufNewFile" },
+
+  custom_surroundings = nil,
+  highlight_duration = 500,
+
+  mappings = {
+    add = 'sa',
+    delete = 'sd',
+    find = 'sf',
+    find_left = 'sF',
+    highlight = 'sh',
+    replace = 'sr',
+    update_n_lines = 'sn',
+
+    suffix_last = 'l',
+    suffix_next = 'n',
+  },
+
+  n_lines = 20,
+  respect_selection_type = false,
+  search_method = 'cover',
+  silent = false,
 }
