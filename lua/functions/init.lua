c61229dbcca6495cf49c003548f02ffd9bd1e3ce
coreytruscott8@gmail.com <coreytruscott8@gmail.com>
2023-05-18 20:49:26 +0100

    add more lsp servers to personal func

--- lua/functions/init.lua
+++ lua/functions/init.lua
@@ -1,9 +1,9 @@
--- install treesitter syntax and lsp servers
+-- installation of lsp servers and stuff just for me
 function TSplusLSPinstaller()
   vim.cmd[[
     e ~/.config/nvim/init.lua
     TSUpdate
     TSInstall all
-    MasonInstall fixjson lua-language-server bash-language-server black clang-format clangd flake8 html-lsp json-lsp pyright rust-analyzer stylua rustfmt
+    MasonInstall fixjson lua-language-server bash-language-server black clang-format clangd flake8 html-lsp json-lsp pyright rust-analyzer stylua rustfmt autoflake css-lsp rnix-lsp yamlfix yaml-language-server
   ]]
 end
