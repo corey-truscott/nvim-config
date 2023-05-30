00bd7d2cbf2665c2fb4fb28108c80fe1d43938fa
coreytruscott8@gmail.com <coreytruscott8@gmail.com>
2023-05-30 15:44:02 +0100

    add colourscheme switch

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,15 +1,31 @@
-return {
-  -- everforest, default theme
-  {
-    "sainnhe/everforest",
-    config = function()
-      vim.cmd [[
-      let g:everforest_background = 'hard'
-      let g:everforest_better_performance = 1
-    ]]
+if Everforest then
+    return {
+        "sainnhe/everforest",
+        config = function()
+            vim.cmd [[
+                  let g:everforest_background = 'hard'
+                  let g:everforest_better_performance = 1
+                  ]]
 
-      Colour = Colour or "everforest"
-      vim.cmd.colorscheme(Colour)
-    end,
-  },
-}
+            Colour = Colour or "everforest"
+            vim.cmd.colorscheme(Colour)
+        end,
+    }
+elseif Tokyonight then
+    return {
+        "folke/tokyonight.nvim",
+        config = function()
+            Colour = Colour or "tokyonight"
+            vim.cmd.colorscheme(Colour)
+        end,
+    }
+elseif Solarized then
+    return {
+        "shaunsingh/solarized.nvim",
+        config = function ()
+            Colour = Colour or "solarized"
+            vim.cmd.colorscheme(Colour)
+        end
+    }
+end
+
--- lua/user_opts/init.lua
+++ lua/user_opts/init.lua
@@ -1,8 +1,19 @@
 -- user options
 
 -- neorg path
-NeorgPath = "~/docs/neorg"
-os.execute("mkdir -p " .. NeorgPath)
+NeorgPath = ""
+
+-- TODO: put this in neorg.lua
+if (NeorgPath ~= "") then
+    os.execute("mkdir -p " .. NeorgPath)
+end
+
+-- colourscheme
+-- only set one to true, otherwise mutliple
+-- colourschemes will be installed causing startup times to be slower
+Everforest = true
+Tokyonight = false
+Solarized = false
 
 -- transparency
 Transparent = false
