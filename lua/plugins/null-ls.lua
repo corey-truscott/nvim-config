e8c7d64be18ce5f03cf99b2e2e1015fc22f2a115
coreytruscott8@gmail.com <coreytruscott8@gmail.com>
2023-05-30 08:28:56 +0100

    lazyload null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,6 +1,6 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
-  -- event = { "BufReadPre", "BufNewFile" },
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = { { "nvim-lua/plenary.nvim", lazy = true } },
   config = function()
     local lsp = require("lsp-zero").preset({})
